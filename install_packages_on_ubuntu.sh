27644187ab8b5e28a4729fb7e91b4653604a9ae1
Circle-Kai <57470091+Circle-Kai@users.noreply.github.com>
2020-03-21 23:07:56 +0800

    Create install_packages_on_ubuntu.sh

---
+++ install_packages_on_ubuntu.sh
@@ -0,0 +1,11 @@
+# Install Packages on Ubuntu
+###### Tags: `ShellScripts` `Ubuntu` `Package`
+#!/usr/bin/env bash
+
+#Base on ubuntu 18.04
+#----------------------------------------------------------------------------------------------------------------------------
+
+sudo apt isntall -y \
+    terminator \
+    exfat-fuse \
+    exfat-utils
